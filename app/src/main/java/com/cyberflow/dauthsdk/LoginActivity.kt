93d5fbacb921892ccc6487771a4a49160d5e1bfa
Robby <344133966@qq.com>
2023-06-13 21:06:16 +0800

    定义钱包未创建时code

--- app/src/main/java/com/cyberflow/dauthsdk/LoginActivity.kt
+++ app/src/main/java/com/cyberflow/dauthsdk/LoginActivity.kt
@@ -24,6 +24,7 @@ private const val GOOGLE = "GOOGLE"
 private const val TWITTER = "TWITTER"
 private const val FACEBOOK = "FACEBOOK"
 private const val ACCOUNT_TYPE_OF_EMAIL = 10
+private const val WALLET_IS_NOT_CREATE = 200001
 
 class LoginActivity : AppCompatActivity() {
     var loginBinding: ActivityLoginLayoutBinding?  = null
@@ -70,7 +71,7 @@ class LoginActivity : AppCompatActivity() {
                 val code = DAuthSDK.instance.loginWithType(GOOGLE, this@LoginActivity)
                 when (code) {
                     0 -> handleLoginSuccess()
-                    200001 -> handleCreateWallet()
+                    WALLET_IS_NOT_CREATE -> handleCreateWallet()
                     else -> handleLoginFailure(code)
                 }
             }
@@ -81,7 +82,7 @@ class LoginActivity : AppCompatActivity() {
                 val code = DAuthSDK.instance.loginWithType(TWITTER, this@LoginActivity)
                 when (code) {
                     0 -> handleLoginSuccess()
-                    200001 -> handleCreateWallet()
+                    WALLET_IS_NOT_CREATE -> handleCreateWallet()
                     else -> handleLoginFailure(code)
                 }
             }
@@ -93,7 +94,7 @@ class LoginActivity : AppCompatActivity() {
                 DAuthLogger.d("EOA钱包登录返回code:$code")
                 when(code) {
                     0 -> MainActivity.launch(this@LoginActivity)
-                    200001 -> handleCreateWallet()
+                    WALLET_IS_NOT_CREATE -> handleCreateWallet()
                     else -> handleLoginFailure(code)
                 }
             }
